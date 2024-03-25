5bebb8ca0ad0ed4300ca3b589d934ae7de5b15cc
Ducucostin29 <147605466+Ducucostin29@users.noreply.github.com>
2024-03-25 12:10:59 +0200

    var4

--- index.js
+++ index.js
@@ -15,5 +15,51 @@ app.set("view engine","ejs");
 
 app.use("/resurse", express.static(__dirname+"/resurse"));
 
+
+//app.get("/",function(req,res){
+//    res.sendFile(__dirname+"/index.html")
+//})
+
+app.get(["/","/home","/index"],function(req,res){
+    res.render("pagini/index");
+});
+
+
+
+
+app.get("/cerere", function(req,res){
+    res.send("<b>Hello!</b> <span style'color:red'> world</span>");
+});
+
+app.get("/data", function(req,res,next){
+    res.write("Data: ");
+    next();
+});
+
+app.get("/data", function(req,res){
+    res.write(""+new Date());
+    res.end();
+});
+
+
+app.get("/suma/:a/:b", function(req,res){
+    var suma=parseInt(req.params.a)+parseInt(req.params.b)
+    res.send(""+suma);
+});
+
+
+
+
+app.get("/*",function(req,res){
+    console.log(req.url)
+    res.render("pagini"+register.url, function(rezHtml, err){
+        console.log(rezHtml);
+        console.log("Eroare",err);
+        res.send(rezHtml);
+    });
+});
+
+
+
 app.listen(8080);
 console.log("Serverul a pornit");
