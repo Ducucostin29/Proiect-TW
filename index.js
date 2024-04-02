eb7a938a9a044dab73f7db4ed366aed1d9e8e4c1
Ducucostin29 <147605466+Ducucostin29@users.noreply.github.com>
2024-04-02 13:10:59 +0300

    var4

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const express = require("express");
-const fs= require('fs');
-// const path=require('path');
+const express = require("express");//include biblioteca express
+const fs= require('fs');//include biblioteca fs sistemul de fisiere poate sa fac chesti primeasca citeasca etc
+const path=require('path');// se uita la cai
 // const sharp=require('sharp');
 // const sass=require('sass');
 // const ejs=require('ejs');
 
 
-app= express();
-console.log("Folder proiect", __dirname);
-console.log("Cale fisier", __filename);
-console.log("Director de lucru", process.cwd());
 
-app.set("view engine","ejs");
+obGlobal={
+    obErori:null
+}
 
-app.use("/resurse", express.static(__dirname+"/resurse"));
+vect_foldere=["temp","temp1"]
+for(let folder of vect_foldere){
+    let caleFolder=path.join(__dirname, folder)
+    if(!fs.existsSync(caleFolder)){
+        fs.mkdirSync(caleFolder);
+    }
+}
+
+app= express();//apeleaza o functie din express , iar app va astepta o valoare
+console.log("Folder proiect", __dirname);//Folderul aplicatiei
+console.log("Cale fisier", __filename);//calea fisier
+console.log("Director de lucru", process.cwd());//Curent working directory , folderul de unde rulam aplicatia
+
+app.set("view engine","ejs");//motorul de temp
+
+app.use("/resurse", express.static(__dirname+"/resurse"));//fac un alias
 
 
 //app.get("/",function(req,res){
@@ -21,14 +34,11 @@ app.use("/resurse", express.static(__dirname+"/resurse"));
 //})
 
 app.get(["/","/home","/index"],function(req,res){
-    res.render("pagini/index");
+    res.render("pagini/index", {ip: req.ip});
 });
 
-
-
-
 app.get("/cerere", function(req,res){
-    res.send("<b>Hello!</b> <span style'color:red'> world</span>");
+    res.send("<b>Hello</b><span style='color:red'> world !</span>");
 });
 
 app.get("/data", function(req,res,next){
@@ -47,19 +57,91 @@ app.get("/suma/:a/:b", function(req,res){
     res.send(""+suma);
 });
 
+app.get("/favicon.ico", function(req, res){
+    res.sendFile(path.join(__dirname,"resurse/imagini/favicon/favicon"))
+})
+
+app.get("/*.ejs", function(req, res){
+    afisareEroare(res,400);
+})
+
+app.get(new RegExp("^\/[A-Za-z\/0-9]*\/$"), function(req, res){
+    afisareEroare(res,403);
+})
 
 
+app.get("/*", function(req, res){
+    //console.log(req.url)
+    try {
+        res.render("pagini"+req.url, function(err, rezHtml){
+            // console.log(rezHtml);
+            // console.log("Eroare:"+err)
 
-app.get("/*",function(req,res){
-    console.log(req.url)
-    res.render("pagini"+register.url, function(err,rezHtml){
-        console.log(rezHtml);
-        console.log("Eroare",err);
-        res.send(rezHtml);
-    });
-});
+                if (err){
+                    if (err.message.startsWith("Failed to lookup view")){
+                        afisareEroare(res,404);
+                        console.log("Nu a gasit pagina: ", req.url)
+                    }
+                    
+                }
+
+            
+        });         
+    }
+    catch (err1){
+        if (err1.message.startsWith("Cannot find module")){
+            afisareEroare(res,404);
+            console.log("Nu a gasit resursa: ", req.url)
+        }
+        else{
+            afisareEroare(res);
+            console.log("Eroare:"+err1)
+        }
+    }
+
+})
+        
 
 
+function initErori(){
+    var continut=fs.readFileSync(path.join(__dirname,"resurse/json/erori.json")).toString("utf-8")
+    console.log(continut);
 
-app.listen(8080);
+    obGlobal.obErori=JSON.parse(continut);
+    for (let eroare of obGlobal.obErori.info_erori){
+        eroare.imagine=path.join(obGlobal.obErori.cale_baza,eroare.imagine)
+    }
+    obGlobal.obErori.eroare_default=path.join(obGlobal.obErori.cale_baza,obGlobal.obErori.eroare_default.imagine)
+    console.log(obGlobal.obErori);
+}
+
+initErori()
+
+function afisareEroare(res,_identificator,_titlu,_text,_imagine){
+    let eroare=obGlobal.obErori.info_erori.find(
+        function(elem){
+            return elem.identificator=_identificator
+        }
+    )
+    if(!eroare){
+        let eroare_default=obGlobal.obErori.eroare_default;
+        res.render("pagini/eroare", {
+            titlu: _titlu || eroare_default.titlu,
+            text: _text || eroare_default.text,
+            imagine: _imagine || eroare_default.imagine,
+        })//al doilea arg este locals
+    }
+    else{
+        if(eroare.status)
+            res.status(eroare.identificator)
+
+        res.render("pagini/eroare", {
+            titlu: _titlu || eroare.titlu,
+            text: _text || eroare.text,
+            imagine: _imagine || eroare.imagine,
+        })
+    }
+}
+
+app.listen(8080);//port de ascultare
 console.log("Serverul a pornit");
